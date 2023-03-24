9248b190775327006355380cdc259611cefab05a
Anastasia <anastasia.bakuryna@gmail.com>
2023-03-24 10:31:03 -0300

    init

---
+++ src/main/java/com/task/ceilingfan/CeilingFanApplication.java
@@ -0,0 +1,12 @@
+package com.task.ceilingfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeilingFanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeilingFanApplication.class, args);
+    }
+}
